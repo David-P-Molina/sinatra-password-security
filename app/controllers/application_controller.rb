c4c3e8b2db013e48f834362cfa93c330a6a1aaf0
David Moina <davemolina777@gmail.com>
2021-01-27 17:54:27 -0600

    commit tree f356fe7c7ffeb3ecf02256c6eb86d8bd7c217264

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,27 +8,40 @@ class ApplicationController < Sinatra::Base
 		set :session_secret, "password_security"
 	end
 
-	get "/" do
+	get "/" do #File with links to signup or login
 		erb :index
 	end
 
-	get "/signup" do
+	get "/signup" do#renders a for to create a new user.
 		erb :signup
 	end
 
 	post "/signup" do
-		#your code here!
-	end
+		user = User.new(:username => params[:username], :password => params[:password])
+	  
+		if user.save
+		  redirect "/login"
+		else
+		  redirect "/failure"
+		end
+	  end 
 
-	get "/login" do
+	get "/login" do#renders a form for logging in
 		erb :login
 	end
 
 	post "/login" do
-		#your code here!
-	end
+		user = User.find_by(:username => params[:username])
 
-	get "/success" do
+		if user && user.authenticate(params[:password])
+		  session[:user_id] = user.id
+		  redirect "/success"
+		else
+		  redirect "/failure"
+		end
+	  end 
+
+	get "/success" do#should be displayed once a user succssfully logs in
 		if logged_in?
 			erb :success
 		else
@@ -36,11 +49,11 @@ class ApplicationController < Sinatra::Base
 		end
 	end
 
-	get "/failure" do
+	get "/failure" do#Will be accessed if there is an error logging in or signing up
 		erb :failure
 	end
 
-	get "/logout" do
+	get "/logout" do#clears the session data and redirects to homepage
 		session.clear
 		redirect "/"
 	end
